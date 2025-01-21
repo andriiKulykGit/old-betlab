fe6e7cb832daddd1120722b2af666664b4691ec1
Andrii <andrii.kulyk.job@gmail.com>
2025-01-21 19:40:34 +0100

    Created new hockey table

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 "use strict"
+
+$(document).ready(function () {
+	$(document).on("click", ".table2__btn", function () {
+		$(this).toggleClass("_active")
+		$(this).closest(".table2__row").next(".table2__group").stop().slideToggle(300)
+	})
+})
